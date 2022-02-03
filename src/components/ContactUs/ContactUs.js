78601933ce90d5ac8bfa7a03642528e8fbac4dbc
Your Name <saifunnahar.sumi97@gmail.com>
2022-02-04 01:45:31 +0600

    ffffirst

--- src/components/ContactUs/ContactUs.js
+++ src/components/ContactUs/ContactUs.js
@@ -1,13 +1,35 @@
-import { Grid } from '@mui/material';
+import { Grid, InputAdornment, TextField, Typography } from '@mui/material';
 import React from 'react';
 import contactImg from '../../img/contact.png';
-import EmailIcon from '@mui/icons-material/Email';
+// import { styled } from "@mui/system";
+import './ContactUs.css'
+import { AccountCircle } from '@mui/icons-material';
+import { ClassNames } from '@emotion/react';
+import { borderRadius, style } from '@mui/system';
 
+// const TextFieldStyled = styled(TextField)({
+
+// })
 const ContactUs = () => {
     return (
         <Grid container spacing={2}>
             <Grid item xs={6}>
-                <h1>Contact me <EmailIcon/></h1>
+                <h1>Contact Us</h1>
+                <hr />
+                <TextField
+                    style={{background: '#fff', borderRadius: '10px', outline: '0', border: 'none'}}
+                    id="outlined-basic"
+                    InputProps={{
+                    startAdornment: (
+                        <InputAdornment position="start"   style={{ outline: '0', border: 'none'}}>
+                        <AccountCircle />
+                        <TextField id="input-with-sx" label="With sx" variant="standard" />
+                        {/* <Typography ml={2}  variant='p' component="p"></Typography> */}
+                        </InputAdornment>
+                    ),
+                    }}
+        variant="outlined"
+      />
             </Grid>
             <Grid item xs={6}>
                 <img src={contactImg} alt=''/>
--- src/components/FAQ/FAQ.js
+++ src/components/FAQ/FAQ.js
@@ -1,6 +1,11 @@
 import { Grid } from '@mui/material';
 import React from 'react';
 import faqImg from '../../img/faq.png'
+import Accordion from '@mui/material/Accordion';
+import AccordionSummary from '@mui/material/AccordionSummary';
+import AccordionDetails from '@mui/material/AccordionDetails';
+import Typography from '@mui/material/Typography';
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 
 const FAQ = () => {
     return (
@@ -8,6 +13,47 @@ const FAQ = () => {
         <Grid container spacing={2}>
             <Grid item xs={6}>
                 <h1>FAQ</h1>
+                <div>
+      <Accordion>
+        <AccordionSummary
+          expandIcon={<ExpandMoreIcon />}
+          aria-controls="panel1a-content"
+          id="panel1a-header"
+        >
+          <Typography>Accordion 1</Typography>
+        </AccordionSummary>
+        <AccordionDetails>
+          <Typography>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
+            malesuada lacus ex, sit amet blandit leo lobortis eget.
+          </Typography>
+        </AccordionDetails>
+      </Accordion>
+      <Accordion>
+        <AccordionSummary
+          expandIcon={<ExpandMoreIcon />}
+          aria-controls="panel2a-content"
+          id="panel2a-header"
+        >
+          <Typography>Accordion 2</Typography>
+        </AccordionSummary>
+        <AccordionDetails>
+          <Typography>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
+            malesuada lacus ex, sit amet blandit leo lobortis eget.
+          </Typography>
+        </AccordionDetails>
+      </Accordion>
+      <Accordion disabled>
+        <AccordionSummary
+          expandIcon={<ExpandMoreIcon />}
+          aria-controls="panel3a-content"
+          id="panel3a-header"
+        >
+          <Typography>Disabled Accordion</Typography>
+        </AccordionSummary>
+      </Accordion>
+    </div>
             </Grid>
             <Grid item xs={6}>
                 <img src={faqImg} alt=''/>
