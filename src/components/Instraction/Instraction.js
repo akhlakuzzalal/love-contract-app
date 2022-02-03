3bc0559e40121fd019f9e7b7a93dbaa98f7c08cd
akhlakuzzalal <akhlakuzzalal@gmail.com>
2022-02-04 01:42:33 +0600

    designed

--- src/components/Instraction/Instraction.js
+++ src/components/Instraction/Instraction.js
@@ -1,16 +1,26 @@
 import { Grid } from '@mui/material';
 import React from 'react';
 import instractionImg from '../../img/instraction.png'
+import './instraction.css'
+import first from '../../img/love-point/love1.png'
 
 const Instraction = () => {
     return (
-        <div>
+        <div className='container'>
             <Grid container spacing={2}>
             <Grid item xs={6}>
-                <h1>Instraction</h1>
+                <h1 style={{display:"flex",alignItems:'center',lineHeight:0}}><span style={{marginRight:'15px'}}><img width={'50px'} src={first} alt="" /></span> Instraction</h1>
+                <ul>
+                    <li className='instract-item'>Unlimited Likes</li>
+                    <li className='instract-item'>See who likes you</li>
+                    <li className='instract-item'>Match Anywhere</li>
+                    <li className='instract-item'>Additional matches</li>
+                    <li className='instract-item'>Rewind features</li>
+                    <li className='instract-item'>Data setup features</li>
+                </ul>
             </Grid>
-            <Grid item xs={6}>
-                <img src={instractionImg} alt=''/>
+            <Grid sx={{display:'flex',justifyContent:'center'}} item xs={6}>
+                <img width={'400px'} src={instractionImg} alt=''/>
             </Grid>
         </Grid>
         </div>
