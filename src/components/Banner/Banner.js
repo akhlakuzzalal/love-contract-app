7ca166073ff38495179bd1da6335302df0e8e8f9
akhlakuzzalal <akhlakuzzalal@gmail.com>
2022-02-04 14:47:56 +0600

    added

--- src/components/Banner/Banner.js
+++ src/components/Banner/Banner.js
@@ -2,10 +2,17 @@ import React from 'react';
 import banner from '../../img/banner-bg.png'
 import './banner.css'
 import cloud from '../../img/cloud.png'
+import YouTubeIcon from '@mui/icons-material/YouTube';
+import InstagramIcon from '@mui/icons-material/Instagram';
+import FacebookIcon from '@mui/icons-material/Facebook';
 
 const Banner = () => {
     return (
         <div className='container'>
+            <YouTubeIcon/>
+            <InstagramIcon/>
+            <FacebookIcon/>
+            
             <img width={'100%'} src={banner} alt='banner'/>
             <div className='banner-cloud'>
                 <img className='banner-img' src={cloud} alt="" />
